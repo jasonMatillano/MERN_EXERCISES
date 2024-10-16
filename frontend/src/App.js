f39d1120a5373ada5fefd9505f5fec12384a33c4
Jason <jason.matillano@gmail.com>
2024-10-17 04:22:14 +0800

    08AddedCSSStyles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className='pages'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-          </Routes>
-        </div>
+        <Navbar/>
+          <div className='pages'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+            </Routes>
+          </div>
       </Router>
     </div>
   );
