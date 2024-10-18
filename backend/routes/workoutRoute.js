7d24dc058f2d2b7f0e0bbf07a709724b9ad60ea5
Jason <jason.matillano@gmail.com>
2024-10-19 02:58:43 +0800

    25AddedWorkoutAuthMiddleware

--- backend/routes/workoutRoute.js
+++ backend/routes/workoutRoute.js
@@ -6,8 +6,12 @@ const  {
     deleteWorkout,
     updateWorkout
 } = require('../controllers/workoutCotroller')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
+
+// require auth for all workout routes
+router.use(requireAuth)
 
 // get all workouts
 router.get('/', getWorkouts)
