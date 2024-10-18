bc0f377a76c2ea15881b89311aeccce9083e38d5
Jason <jason.matillano@gmail.com>
2024-10-18 21:51:21 +0800

    18CreatedJWTWebToken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+// create token
+const maxAge = 3 * 24 * 60 * 60 // 3 days
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: maxAge })
+}
 
 // loging user
 const loginUser = async (req, res) => {
@@ -11,7 +18,11 @@ const signupUser = async (req, res) => {
     
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+        
+        // create token
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+
     } catch (error) {
         res.status(400).json({error: error.message})
     }
