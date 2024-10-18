d28fc2030579d02b6a571dfcfe8d9b341f53e357
Jason <jason.matillano@gmail.com>
2024-10-19 00:58:35 +0800

    22AddedSignUpHook

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,14 +1,17 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
+
 
 const Signup = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { signup, error, isLoading } = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()  
 
-        console.log(email, password)
+        await signup(email, password)
     }
 
     return (
@@ -28,7 +31,8 @@ const Signup = () => {
                 onChange={(e) => setPassword(e.target.value)}
             />
 
-            <button type="submit">Signup</button>
+            <button disabled={isLoading} type="submit">Signup</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
